6a521011f9760fb7759f1cf60da19ebc1ebb6684
gabrielosantosb <email@email.com>
2024-01-12 16:38:38 -0300

    Criando tela home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,15 @@
+
 import React from "react";
-import { Container, Description, ProfessionalSummary, Title } from "./styles";
-import { AnimatedPages, StyledFadeIn } from "../../styles/global";
-import { useSpring, animated } from "react-spring";
+import {
+  Container,
+  ProfessionalSummary,
+  Title,
+  Description,
+  SkillsContainer,
+  SkillCard,
+  ContactContainer,
+  ContactCard,
+} from "./styles";
 import { FadeIn } from "../../components/fadeIn";
 
 export const Home = () => {
@@ -9,15 +17,39 @@ export const Home = () => {
     <FadeIn>
       <Container>
         <ProfessionalSummary>
-          <Title>Professional Summary</Title>
+          <Title>Gabriel Barbosa dos Santos</Title>
           <Description>
-            Experienced and passionate web developer with a strong background in
-            front-end technologies. Proven ability to design and implement
-            responsive and user-friendly interfaces. Detail-oriented with
-            excellent problem-solving skills. Adept at collaborating with
-            cross-functional teams to drive project success.
+            Estudante de Ciências da Computação, entusiasta da programação,
+            dedicado a buscar oportunidades para crescer profissionalmente e aprofundar
+            conhecimentos em tecnologias modernas. Possuo facilidade de aprendizado,
+            habilidades em comunicação e um comprometimento constante com a evolução pessoal.
           </Description>
         </ProfessionalSummary>
+
+        <SkillsContainer>
+          <SkillCard>
+            <h3>Habilidades</h3>
+            <ul>
+              <li>Lógica de Programação</li>
+              <li>React Native</li>
+              <li>Angular</li>              
+              <li>SQL</li>
+              <li>Postman</li>
+              <li>.NET</li>                                                                            
+            </ul>
+          </SkillCard>
+        <ContactContainer>
+          <ContactCard>
+            <h3>Contato</h3>
+            <ul>
+              <li>Website: <a href="https://portfoliopessoal-kappa.vercel.app" target="_blank" rel="noopener noreferrer">portfoliopessoal-kappa.vercel.app</a></li>
+              <li>GitHub: <a href="https://github.com/Gabrielosantosb" target="_blank" rel="noopener noreferrer">Gabrielosantosb</a></li>
+            </ul>
+          </ContactCard>
+        </ContactContainer>
+        </SkillsContainer>
+
+
       </Container>
     </FadeIn>
   );
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,26 +1,98 @@
+// styles.js
 import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  height: 100vh;
 `;
 
 export const ProfessionalSummary = styled.div`
-max-width: 600px;
-text-align: center;
+  max-width: 800px;
+  text-align: center;
+  margin-bottom: 40px;
 `;
 
 export const Title = styled.h2`
-color: #3498db;
-font-size: 28px;
-margin-bottom: 20px;
+  color: ${(props) => props.theme.textColor};
+  font-size: 28px;
+  margin-bottom: 20px;
 `;
 
 export const Description = styled.p`
-font-size: 18px;
-color: #555;
-line-height: 1.6;
+  font-size: 18px;
+  color: ${(props) => props.theme.textColor};
+  line-height: 1.6;
+`;
+
+export const SkillsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+export const SkillCard = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+  color: ${(props) => props.theme.textColor};
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 0 20px;
+  max-width: 300px;
+
+  h3 {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 8px;
+  }
+`;
+
+export const ContactContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`;
+
+export const ContactCard = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+  color: ${(props) => props.theme.textColor};
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 0 20px;
+  max-width: 300px;
+
+  h3 {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 8px;
+  }
+
+  a {
+    color: ${(props) => props.theme.linkTextColor};
+    text-decoration: none;
+    &:hover {
+      color: ${(props) => props.theme.linkHoverTextColor};
+    }
+  }
 `;
