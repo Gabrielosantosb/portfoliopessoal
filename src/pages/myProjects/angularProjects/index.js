35cb72bfd53f056c7c3e5e2b0a151315e80bde3b
gabrielosantosb <email@email.com>
2024-01-12 14:19:07 -0300

    limpando codigo

--- src/pages/myProjects/angularProjects/index.js
+++ src/pages/myProjects/angularProjects/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import {
   HeaderContainer,
   ProjectTitle,
-  PageContainer,
   ProjectsContainer,
-  ProjectCard,
-  Title,
-  ImageContainer,
-  SubTitle,
 } from "../styles";
 import { GalleryAngularImages } from "../../../components/galleryImages/galleryAngular";
 
--- src/pages/myProjects/backProjects/index.js
+++ src/pages/myProjects/backProjects/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {
     HeaderContainer,
-    ProjectTitle,
-    PageContainer,
+    ProjectTitle,    
     ProjectsContainer,
     ProjectCard,
     Title,
